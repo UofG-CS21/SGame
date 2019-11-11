f49a62b4ad91ceac37c0997072bfa7510bee23bb
Samuel Gurský <samkogursky@gmail.com>
2019-11-11 12:49:08 +0100

    add argparse to sample_client
    Client needed options for host and port; added.

--- SampleClient.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-print('Sending connect request...')
-response = requests.post(url='http://localhost:8000/connect')
-data = {'token' : response.json()['token']}
-print('Received session token ' + response.json()['token'])
-print('Sending disconnect request...')
-response = requests.post(url = 'http://localhost:8000/disconnect', json = data)
-print('Received response: ' + response.text)
