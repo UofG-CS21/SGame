7a964a3577f4f61087dd8de3aba5987b1e184554
Paolo Jovon <paolo.jovon@gmail.com>
2019-11-19 22:07:45 +0000

    Added a RestApi attribute for handler methods
    Reason for change: Preparation for the future automated REST API parser
    system.

---
+++ SGame/REST.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SGame
+{
+    /// <summary>
+    /// An attribute used to mark REST API handler methods.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    class RestApi : Attribute
+    {
+    }
+}
