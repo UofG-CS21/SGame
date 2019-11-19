e2fb7e6861e72131ab9e58a855aeb26a4d984430
Matthew Walker <2310528w@student.gla.ac.uk>
2019-11-19 21:46:03 +0000

    Added attribute for tests to use protected members
    Reason for change: See Merge request !9 discussion

--- SGame.Tests/SGame_MainShould.cs
+++ SGame.Tests/SGame_MainShould.cs
@@ -3,20 +3,7 @@ using CommandLine;
 using SGame;
 using Xunit;
 
-namespace SGame.UnitTests {
-    class CmdLineOptions {
-        /// <summary>
-        /// The HTTP hostname to bind to.
-        /// </summary>
-        [Option ('H', "host", Default = "localhost", Required = false, HelpText = "The hostname to bind the compute node to.")]
-        public string Host { get; set; }
-
-        /// <summary>
-        /// The HTTP TCP port to bind to.
-        /// </summary>
-        [Option ('P', "port", Default = 8000u, Required = false, HelpText = "The port to bind the compute node to.")]
-        public uint Port { get; set; }
-    }
+namespace SGame.Tests {
 
     /// <summary>
     /// Tests that cmd line options are correctly parsed
