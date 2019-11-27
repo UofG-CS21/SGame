020d618f34a7e972f4b4fa9d69cf7a7355dabc5b
Matthew Walker <2310528w@student.gla.ac.uk>
2019-11-27 13:28:49 +0000

    Added more attr tests
    Reason for change: We needed more tests.
    
    !14

--- SGame.Tests/SGame_AttrTests.cs
+++ SGame.Tests/SGame_AttrTests.cs
@@ -12,19 +12,29 @@ namespace SGame.Tests
     {
         [Fact]
         [ApiRoute("Route 17")]
-        public void TestThatRouteIsRoute17()
+        public void TestThatRouteIsCorrect()
         {
             TestAttrProperty<ApiRoute, string>(new StackFrame().GetMethod(),
                          "Route", "Route 17");
         }
 
-        // Helpers
+        [Fact]
+        [ApiParam("Apple", typeof(string))]
+        public void TestThatParamIsCorrect()
+        {
+            TestAttrProperty<ApiParam, string>(new StackFrame().GetMethod(),
+                         "Name", "Apple");
+            TestAttrProperty<ApiParam, System.Type>(new StackFrame().GetMethod(),
+                         "Type", typeof(string));
+        }
+
+        // Helpers Inspired by code from: https://www.codeproject.com/Articles/231959/Unit-Testing-Csharp-Custom-Attributes-with-NUnit-P
         private void TestAttrProperty<TAttr, TProp>(MethodBase method,
                      string argName, TProp expectedValue)
         {
             object[] customAttributes = method.GetCustomAttributes(typeof(TAttr), false);
 
-            Assert.Equal(1, customAttributes.Length);
+            Assert.Single(customAttributes);
 
             TAttr attr = (TAttr)customAttributes[0];
 
