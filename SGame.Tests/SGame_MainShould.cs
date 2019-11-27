9185715a99ec0e6573bc0d15a88c5c6c7cb614c3
Matthew Walker <2310528w@student.gla.ac.uk>
2019-11-27 14:10:26 +0000

    Added template for Request/Response tests

--- SGame.Tests/SGame_MainShould.cs
+++ SGame.Tests/SGame_MainShould.cs
@@ -2,27 +2,43 @@ using System;
 using CommandLine;
 using SGame;
 using Xunit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
-namespace SGame.Tests {
+namespace SGame.Tests
+{
+
+
+    public class RequestResponseTest
+    {
+        [Theory]
+        public void ResponseDataTest(JObject json, string requestUrl){
+
+        }
+    }
 
     /// <summary>
     /// Tests that cmd line options are correctly parsed
     /// </summary>
-    public class CmdLineOptions_ParseTester {
+    public class CmdLineOptions_ParseTester
+    {
         private readonly string[] args;
         private readonly string Hostname = "TestHost";
         private readonly uint Port = 6000;
 
-        public CmdLineOptions_ParseTester () {
-            this.args = new string[] { "-H", Hostname, "-P", Port.ToString () };
+        public CmdLineOptions_ParseTester()
+        {
+            this.args = new string[] { "-H", Hostname, "-P", Port.ToString() };
         }
 
         [Fact]
-        public void ParseTest () {
-            Parser.Default.ParseArguments<CmdLineOptions> (this.args)
-                .WithParsed<CmdLineOptions> (opts => {
-                    Assert.Equal (opts.Host, this.Hostname);
-                    Assert.Equal (opts.Port, this.Port);
+        public void ParseTest()
+        {
+            Parser.Default.ParseArguments<CmdLineOptions>(this.args)
+                .WithParsed<CmdLineOptions>(opts =>
+                {
+                    Assert.Equal(opts.Host, this.Hostname);
+                    Assert.Equal(opts.Port, this.Port);
                 });
 
         }
