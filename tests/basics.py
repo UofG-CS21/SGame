4d66e9ac7130d76de8dd41dbe101b54ff921ea77
Mustafaa <mustafaa.ahmad@hotmail.co.uk>
2019-11-27 12:59:32 +0000

    WIP:  Working on movement test
    Reason for change: Adding new test for movement

--- tests/basics.py
+++ tests/basics.py
@@ -1,4 +1,3 @@
-import pytest
 import requests
 
 
@@ -6,5 +5,21 @@ def test_disconnect(client):
     """
     Tests that a connected ship can disconnect via REST.
     """
-    resp = requests.post(client.url + 'disconnect', json={'token': client.token})
+    resp = requests.post(client.url + 'disconnect',
+                         json={'token': client.token})
+    assert resp
+
+
+def test_movement(client):
+    resp = requests.post(client.url + 'accelerate', json={
+        'token': client.token,
+        'x': 23.6,
+        'y': 45.5,
+    })
+    assert resp
+
+    # TODO:  Check if the acceleration matches the expected position, and add comments
+
+    resp = requests.post(client.url + 'disconnect',
+                         json={'token': client.token})
     assert resp
