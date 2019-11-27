80afc680d09004ceb5cfa90d30e4cc7d5dfac980
Mustafaa <mustafaa.ahmad@hotmail.co.uk>
2019-11-27 13:21:42 +0000

    Completed  movement test
    Reason for change: Finished test, to include getShipInfo

--- tests/basics.py
+++ tests/basics.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 
@@ -11,15 +12,27 @@ def test_disconnect(client):
 
 
 def test_movement(client):
+    # Test values for the coordinates
+    x = 23.6
+    y = 45.5
+    # Testing accelerate with the given x and y values
     resp = requests.post(client.url + 'accelerate', json={
         'token': client.token,
-        'x': 23.6,
-        'y': 45.5,
+        'x': x,
+        'y': y,
+    })
+    assert resp
+    # Using the getShipInfo to test the values are correct
+    resp = requests.post(client.url + 'getShipInfo', json={
+        'token': client.token,
     })
     assert resp
 
-    # TODO:  Check if the acceleration matches the expected position, and add comments
+    resp_data = resp.json()
+    assert resp_data['x'] == x
+    assert resp_data['y'] == y
 
+    # Disconnect
     resp = requests.post(client.url + 'disconnect',
                          json={'token': client.token})
     assert resp
