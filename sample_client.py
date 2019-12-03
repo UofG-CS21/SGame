1d3668a223aa4573d53228774cdba8c973e769ef
Samuel Gursky <2335488g@student.gla.ac.uk>
2019-12-03 11:07:22 +0100

    add spaceship.Update, Api.UpdateState, Api.Accelerate uses energy
    spaceship.Update updates spaceship energy and position based on time elapsed since last update
    Api.UpdateState updates all connected spaceships
        using System.Diagnostics.Stopwatch to measure gameTime
    Api.AcceleratePlayer uses spaceship energy, and accelerates less if not enough energy is available

--- sample_client.py
+++ sample_client.py
@@ -1,8 +1,9 @@
 import requests
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-P', '--port', type=int, default = 8000, help='Port to connect to')
+parser.add_argument('-P', '--port', type=int, default = 5000, help='Port to connect to')
 parser.add_argument('-H', '--host', type=str, default = 'localhost', help = 'Host to connect to')
 
 args = parser.parse_args()
@@ -11,8 +12,23 @@ host = getattr(args,'host')
 
 print('Sending connect request...')
 response = requests.post(url='http://' + host + ':' + str(port) + '/connect')
-data = {'token' : response.json()['token']}
-print('Received session token ' + response.json()['token'])
+token = response.json()['token']
+print('Received session token ' + token)
+
+data = {'token' : token, 'x' : 1, 'y' : 2}
+print('Accelerating...')
+response = requests.post(url = 'http://' + host + ':' + str(port) + '/accelerate', json = data)
+
+
+print('Waiting a second...')
+time.sleep(1)
+
+data = {'token' : token}
+print('Getting state')
+response = requests.post(url = 'http://' + host + ':' + str(port) + '/getShipInfo', json = data)
+print(response.json())
+
+data = {'token' : token}
 print('Sending disconnect request...')
 response = requests.post(url = 'http://' + host + ':' + str(port) + '/disconnect', json = data)
 print('Received response: ' + response.text)
