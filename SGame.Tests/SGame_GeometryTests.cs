eb960e9648eb525d7b41e30d6361e4456546c35b
Matthew Walker <2310528w@student.gla.ac.uk>
2020-01-03 13:42:00 +0000

    CircleTraiangleIntersection xunit tests added

--- SGame.Tests/SGame_GeometryTests.cs
+++ SGame.Tests/SGame_GeometryTests.cs
@@ -40,8 +40,27 @@ namespace SGame.Tests
             Assert.Equal(expected_sign, actual_sign);
         }
 
+        [Theory]
+        [ClassData(typeof(CTSITestData))]
+        public void CircleTriangleSideIntersection(Vector2 circleCenter, double radius, Vector2 linePoint1, Vector2 linePoint2, bool expected)
+        {
+            bool actual = new Api().CircleTriangleSideIntersection(circleCenter, radius, linePoint1, linePoint2);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [ClassData(typeof(CTITestData))]
+        public void CircleTriangleIntersection(Vector2 circleCenter, double radius, Vector2 A, Vector2 B, Vector2 C, bool expected)
+        {
+            bool actual = new Api().CircleTriangleIntersection(circleCenter, radius, A, B, C);
+            Assert.Equal(expected, actual);
+        }
+
+        
     }
 
+
+    //Everything after this is test data. Not sure if this is the cleanest way of storing it.
     public class pLSTestData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
@@ -52,6 +71,63 @@ namespace SGame.Tests
             yield return new object[] { new Vector2(0, -10), new Vector2(-5, 0), new Vector2(5, 0), -1 };
             yield return new object[] { new Vector2(0, 0), new Vector2(-5, 0), new Vector2(5, 0), 0 };
             yield return new object[] { new Vector2(12, 15), new Vector2(-4, 16), new Vector2(8, 12), 1 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class CTSITestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //1 intersection
+            yield return new object[] { new Vector2(-5, -6), 2, new Vector2(-13, -8), new Vector2(3, -8), true };
+            yield return new object[] { new Vector2(-5, -6), 2, new Vector2(-13, -8), new Vector2(-5, 0), false };
+            yield return new object[] { new Vector2(-5, -6), 2, new Vector2(-5, 0), new Vector2(3, -8), false };
+
+            //2 intersections
+            yield return new object[] { new Vector2(-7, -2), 2, new Vector2(-13, -6), new Vector2(-1, -6), false };
+            yield return new object[] { new Vector2(-7, -2), 2, new Vector2(-7, 0), new Vector2(-1, -6), true };
+            yield return new object[] { new Vector2(-7, -2), 2, new Vector2(-13, -6), new Vector2(-7, 0), true };
+
+            //Triangle is completely within circle.
+            yield return new object[] { new Vector2(-3, -2), 4, new Vector2(-6, -3), new Vector2(0, -3), true };
+            yield return new object[] { new Vector2(-3, -2), 4, new Vector2(-3, 0), new Vector2(0, -3), true };
+            yield return new object[] { new Vector2(-3, -2), 4, new Vector2(-6, -3), new Vector2(-3, 0), true };
+
+            //Triangle is completly outwith circle.
+            yield return new object[] { new Vector2(5, -4), 4, new Vector2(-6, -3), new Vector2(0, -3), false };
+            yield return new object[] { new Vector2(5, -4), 4, new Vector2(-3, 0), new Vector2(0, -3), false };
+            yield return new object[] { new Vector2(5, -4), 4, new Vector2(-6, -3), new Vector2(-3, 0), false };
+
+            //Circle is completely within triangle.
+            yield return new object[] { new Vector2(-3, -2), 1, new Vector2(-7, -4), new Vector2(1, -4), false };
+            yield return new object[] { new Vector2(-3, -2), 1, new Vector2(-3, 0), new Vector2(1, -4), false };
+            yield return new object[] { new Vector2(-3, -2), 1, new Vector2(-7, -4), new Vector2(-3, 0), false };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class CTITestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            //1 intersection
+            yield return new object[] { new Vector2(-5, -6), 2, new Vector2(-13, -8), new Vector2(-5, 0), new Vector2(3, -8), true };
+
+            //2 intersections
+            yield return new object[] { new Vector2(-7, -2), 2, new Vector2(-13, -6), new Vector2(-7, 0), new Vector2(-1, -6), true };
+
+            //Triangle is completely within circle.
+            yield return new object[] { new Vector2(-3, -2), 4, new Vector2(-6, -3), new Vector2(-3, 0), new Vector2(0, -3), true };
+
+            //Triangle is completly outwith circle.
+            yield return new object[] { new Vector2(5, -4), 4, new Vector2(-6, -3), new Vector2(-3, 0), new Vector2(0, -3), false };
+
+            //Circle is completely within triangle.
+            yield return new object[] { new Vector2(-3, -2), 1, new Vector2(-7, -4), new Vector2(-3, 0), new Vector2(1, -4), true };
+
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
