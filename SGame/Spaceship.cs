d8b0de723f009d9f658cc3634e3baf9315031581
Paolo Jovon <2328339j@student.gla.ac.uk>
2020-01-15 13:24:46 +0000

    Added ShieldDir and ShieldWidth fields to Spaceship
    Reason for change: Preparations for shielding code.

--- SGame/Spaceship.cs
+++ SGame/Spaceship.cs
@@ -46,7 +46,57 @@ namespace SGame
         /// </summary>
         public int HitPoints { get; set; }
 
-        public Stopwatch gameTime { get; set; }
+        /// <summary>
+        /// Normalizes an angle in radians, i.e. makes it positive and between 0 and 2pi.
+        /// </summary>
+        private static double NormalizeAngle(double angle)
+        {
+            angle = angle % (2.0 * Math.PI);
+            if (angle < 0.0) angle = (2.0 * Math.PI) - angle;
+            return angle;
+        }
+
+        private double _shieldDir;
+
+        /// <summary>
+        /// Direction towards which to shield, in radians relative to world coordinates.
+        /// The shield extends for `ShieldWidth` radians clockwise + `ShieldWidth` radians counterclockwise.
+        /// Automatically clamped to 0..2pi when setting it.
+        /// </summary>
+        public double ShieldDir
+        {
+            get
+            {
+                return _shieldDir;
+            }
+            set
+            {
+                _shieldDir = NormalizeAngle(value);
+            }
+        }
+
+        private double _shieldWidth;
+
+        /// <summary>
+        /// The shield half extents, in radians (see `ShieldDir`'s documentation).
+        /// Automatically clamped to 0..2pi when setting it.
+        /// </summary>
+        public double ShieldWidth
+        {
+            get
+            {
+                return _shieldWidth;
+            }
+            set
+            {
+                _shieldWidth = NormalizeAngle(value);
+            }
+        }
+
+        /// <summary>
+        /// Used to keep track of time deltas.
+        /// </summary>
+        public Stopwatch GameTime { get; set; }
 
         /// <summary>
         /// Timestamp of last time the ship's state was updated
@@ -55,7 +105,7 @@ namespace SGame
 
         public Spaceship(int id, Stopwatch gameTime)
         {
-            this.gameTime = gameTime;
+            this.GameTime = gameTime;
             this.Id = id;
             this.Area = 1;
             this.Energy = 10.0;
@@ -64,11 +114,13 @@ namespace SGame
             this.LastUpdate = gameTime.ElapsedMilliseconds;
             this.MaxHitPoints = (int)(100 + (this.Area * this.Area * 0.2) + (this.Area));
             this.HitPoints = this.MaxHitPoints;
+            this.ShieldDir = 0.0;
+            this.ShieldWidth = 0.0;
         }
 
         public void UpdateState()
         {
-            long time = gameTime.ElapsedMilliseconds;
+            long time = GameTime.ElapsedMilliseconds;
             double elapsedSeconds = (double)(time - LastUpdate) / 1000;
             Pos += Vector2.Multiply(Velocity, (float)elapsedSeconds);
             Energy = Math.Min(Area * 10, Energy + elapsedSeconds * Area);
