fa3e15f6461f32dac2a81b965b2cef0a6f2274bf
Paolo Jovon <2328339j@student.gla.ac.uk>
2020-01-26 19:49:29 +0000

    Stub out command-line parsing for SArbiter
    Reason for change: Need argument handling as in SGame

--- SArbiter/Program.cs
+++ SArbiter/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using CommandLine;
 
 namespace SArbiter
 {
+
+    /// <summary>
+    /// Command-line arguments passed to the arbiter.
+    /// </summary>
+    class CmdLineOptions
+    {
+        /// <summary>
+        /// The HTTP hostname to bind to for clients.
+        /// </summary>
+        [Option('H', "client-host", Default = "localhost", Required = false, HelpText = "The hostname to serve the REST API on.")]
+        public string CientHost { get; set; }
+
+        /// <summary>
+        /// The HTTP TCP port to bind to for clients.
+        /// </summary>
+        [Option('P', "client-port", Default = 8000u, Required = false, HelpText = "The port to bind to to serve the REST API.")]
+        public uint ClientPort { get; set; }
+
+        /// <summary>
+        /// The UDP port to use for the master event bus.
+        /// </summary>
+        [Option("bus-port", Default = 3000u, Required = false, HelpText = "The UDP port to use for the master event bus.")]
+        public uint BusPost { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Parser.Default.ParseArguments<CmdLineOptions>(args)
+                .WithParsed<CmdLineOptions>(opts =>
+                {
+                });
         }
     }
 }
