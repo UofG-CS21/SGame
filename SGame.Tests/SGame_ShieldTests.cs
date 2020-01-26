6b89bdbc6e89962714acec11637808d86fe0167e
Matthew Walker <2310528w@student.gla.ac.uk>
2020-01-26 15:59:16 +0000

    Created Shield tests file.
    Reason for change: Seperates the tests into classes.

---
+++ SGame.Tests/SGame_ShieldTests.cs
@@ -0,0 +1,17 @@
+using System;
+using CommandLine;
+using SGame;
+using Xunit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SGame.Tests
+{
+
+    public class SGame_ShieldTests
+    {
+
+
+        
+    }
+}
