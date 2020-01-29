2f0a1dadaac199e7a9ba4e1bad716428bf2b3c92
Paolo Jovon <2328339j@student.gla.ac.uk>
2020-01-29 16:06:51 +0000

    Remove verbose output from CI script
    Reason for change: It was verbose to the point of being unreadable at times.

--- ci/runtests.sh
+++ ci/runtests.sh
@@ -8,9 +8,6 @@ SGAME_PORT=$2
 # Timeout before "Listnening..." is seen on the stdout of dotnet run
 DOTNET_TIMEOUT=30
 
-# Verbose output
-set -x
-
 # Start an instance of the SGame server in the background (redirect stdout->stderr; sleep a bit for it to init)
 # Store the PID of the background process in a file (SGame.pid) to know what process to kill after.
 # (See issue #33 on why this is needed)
@@ -33,12 +30,13 @@ popd
 # Kill the background process in any case (tests succeeded or failure)
 # (See issue #33; otherwise GitLab will stall until timeout because the SGame process in the background won't terminate!)
 # First try to exit gracefully...
+echo "Send 'exit' command to SGame..."
 curl -X POST -d "exit" "http://${SGAME_HOST}:${SGAME_PORT}/exit" || echo "curl failed to POST exit command"
 sleep 2
 # ...then use brute force if the server still hasn't terminated
 SGAME_PID=$(cat SGame.pid)
-echo "Kill ${SGAME_PID}"
-pkill -KILL ${SGAME_PID} || echo "Server already stopped"
+echo "pkill SGame (PID=${SGAME_PID})"
+pkill -KILL ${SGAME_PID} || echo "  SGame was already stopped"
 
 echo "===== Server output ======================================================"
 cat SGame.out
@@ -49,7 +47,7 @@ wait %1
 # Check if the error code is equal to 0 (Meaning that it has)
 if [ $? -ne 0 ]
  then
-    echo "Server has crashed"
+    echo "SGame exited with error exit code"
     # Exit with this code 
     exit $?
 fi
