198648d781188bdd023fefceb39dd2606f0d463f
Paolo Jovon <paolo.jovon@gmail.com>
2020-02-16 18:05:11 +0000

    Rename BusMaster to BusNode
    Reason for change: Master and Client nodes follow the same interface,
    they just have marginally different initialization code...

--- SShared/BusNode.cs
+++ SShared/BusNode.cs
@@ -4,10 +4,9 @@ using System.Runtime.InteropServices;
 namespace SShared
 {
     /// <summary>
-    /// A master node in the event bus.
-    /// Usually associated with the `SArbiter` instance controlling the event bus.
+    /// A node in the event bus (either master/SArbiter or client/SGame).
     /// </summary>
-    public sealed class BusMaster : IDisposable
+    public sealed class BusNode : IDisposable
     {
         /// <summary>
         /// Number of milliseconds after which to timeout when waiting for ENet events.
@@ -20,7 +19,13 @@ namespace SShared
         /// </summary>
         public ENet.Host Host { get; private set; }
 
-        public BusMaster(string hostname, ushort port, uint maxPeers)
+        /// <summary>
+        /// Creates a new bus node.
+        /// </summary>
+        /// <param name="hostname">Set it to a hostname to connect to if this is a client node, or null if this is a bus master.</param>
+        /// <param name="port">The UDP port to communicate over.</param>
+        /// <param name="maxPeers">The maximum number of clients or peers that this node will be able to communicate with.</param>
+        public BusNode(String hostname, ushort port, uint maxPeers)
         {
             if (!BusContext.Instance.Inited)
             {
@@ -30,10 +35,14 @@ namespace SShared
             Host = new ENet.Host();
 
             ENet.Address addr = new ENet.Address();
-            if (!addr.SetHost(hostname))
+            if (hostname != null)
             {
-                throw new InvalidOperationException($"Could not bind to hostname: {hostname}");
+                if (!addr.SetHost(hostname))
+                {
+                    throw new InvalidOperationException($"Could not set host: {hostname}");
+                }
             }
+            // Do not `SetHost` or `SetIP`
             addr.Port = port;
 
             Host.Create(addr, (int)maxPeers);
@@ -50,24 +59,24 @@ namespace SShared
         }
 
         /// <summary>
-        /// Emitted when a peer (client) connects to the bus.
+        /// Emitted when a peer connects to the bus.
         /// </summary>
-        public event BusPeerEventHandler OnPeerConnect;
+        public event BusPeerEventHandler OnConnect;
 
         /// <summary>
-        /// Emitted when a peer (client) disconnects from the bus.
+        /// Emitted when a peer disconnects from the bus.
         /// </summary>
-        public event BusPeerEventHandler OnPeerDisconnect;
+        public event BusPeerEventHandler OnDisconnect;
 
         /// <summary>
-        /// Emitted when a peer (client) times out.
+        /// Emitted when a peer times out.
         /// </summary>
-        public event BusPeerEventHandler OnPeerTimeout;
+        public event BusPeerEventHandler OnTimeout;
 
         /// <summary>
-        /// Emitted when receiving data from a peer (client).
+        /// Emitted when receiving data from a peer.
         /// </summary>
-        public event BusPeerDataEventHandler OnPeerReceive;
+        public event BusPeerDataEventHandler OnReceive;
 
         /// <summary>
         /// Pool for any new ENet events (triggering an appropriate )
@@ -97,18 +106,18 @@ namespace SShared
                 switch (evt.Type)
                 {
                     case ENet.EventType.Connect:
-                        OnPeerConnect(this, new BusPeerEventArgs(evt.Peer));
+                        OnConnect(this, new BusPeerEventArgs(evt.Peer));
                         break;
                     case ENet.EventType.Disconnect:
-                        OnPeerDisconnect(this, new BusPeerEventArgs(evt.Peer));
+                        OnDisconnect(this, new BusPeerEventArgs(evt.Peer));
                         break;
                     case ENet.EventType.Timeout:
-                        OnPeerTimeout(this, new BusPeerEventArgs(evt.Peer));
+                        OnTimeout(this, new BusPeerEventArgs(evt.Peer));
                         break;
                     case ENet.EventType.Receive:
                         using (var evtArgs = new BusPeerDataEventArgs(evt.Peer, evt.ChannelID, evt.Packet))
                         {
-                            OnPeerReceive(this, evtArgs);
+                            OnReceive(this, evtArgs);
                         }
                         break;
                     default:
@@ -118,14 +127,35 @@ namespace SShared
         }
 
         /// <summary>
-        /// Convenience function. Creates a ENet packet with the given contents
-        /// and broadcasts it to all ENet peers connected to the given channel.
+        /// Creates a ENet packet with the given contents and broadcasts it to all ENet peers connected to the given channel.
         /// </summary>
-        public void Broadcast(byte channel, byte[] data)
+        public void Broadcast(byte[] data, int length, byte channel, ENet.PacketFlags flags = ENet.PacketFlags.None)
         {
             ENet.Packet packet = default(ENet.Packet);
-            packet.Create(data);
+            packet.Create(data, length, flags);
             Host.Broadcast(channel, ref packet);
+            packet.Dispose();
+        }
+
+        /// <summary>
+        /// Creates a ENet packet with the given contents and sends it to all given peers over the given channel.
+        /// </summary>
+        public void Multicast(byte[] data, int length, byte channel, ENet.Peer[] peers, ENet.PacketFlags flags = ENet.PacketFlags.None)
+        {
+            ENet.Packet packet = default(ENet.Packet);
+            packet.Create(data, length, flags);
+            Host.Broadcast(channel, ref packet, peers);
+            packet.Dispose();
+        }
+
+        /// <summary>
+        /// Creates a ENet packet with the given contents and sends it to the given peer.
+        /// </summary>
+        public static void Send(byte[] data, int length, byte channel, ENet.Peer target, ENet.PacketFlags flags = ENet.PacketFlags.None)
+        {
+            ENet.Packet packet = default(ENet.Packet);
+            packet.Create(data, length, flags);
+            target.Send(channel, ref packet);
             packet.Dispose();
         }
     }
